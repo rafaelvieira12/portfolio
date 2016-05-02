7cbe2b507d6941ec88e3e6db83924826c5a003ef
Thomaz <thomaz.pupo@viewit.com.br>
2016-05-02 10:36:16 -0300

    site atual

---
+++ Default.aspx.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        panelNome.Visible = false;
+        panelEmail.Visible = false;
+        panelAssunto.Visible = false;
+        panelMensagem.Visible = false;
+        panelMsgEnviada.Visible = false;
+    }
+
+    protected void btEnviar_Click(object sender, EventArgs e)
+    {
+        bool campos = true;
+
+        if (string.IsNullOrEmpty(txtNome.Text))
+        {
+            panelNome.Visible = true;
+            erroNome.Text = "Obrigatório preenchimento do Nome.";
+            campos = false;
+        }
+        else if (txtNome.Text.Length <= 5)
+        {
+            panelNome.Visible = true;
+            erroNome.Text = "Digite pelo menos 5 letras no campo Nome.";
+            campos = false;
+        }
+        else
+        {
+            panelNome.Visible = false;
+        }
+
+        if (string.IsNullOrEmpty(txtSobreNome.Text))
+        {
+            panelSobreNome.Visible = true;
+            erroSobreNome.Text = "Obrigatório preenchimento do Sobre Nome.";
+            campos = false;
+        }
+        else if (txtSobreNome.Text.Length <= 5)
+        {
+            panelSobreNome.Visible = true;
+            erroSobreNome.Text = "Digite pelo menos 5 letras no campo Sobre Nome.";
+            campos = false;
+        }
+        else
+        {
+            panelSobreNome.Visible = false;
+        }
+
+        if (string.IsNullOrEmpty(txtEmail.Text))
+        {
+            panelEmail.Visible = true;
+            erroEmail.Text = "Obrigatório preenchimento do E-mail.";
+            campos = false;
+        }
+        else
+        {
+            Regex email = new Regex(@"^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$");
+
+            if (email.IsMatch(txtEmail.Text))
+            {
+                panelEmail.Visible = false;
+            }
+            else
+            {
+                panelEmail.Visible = true;
+                erroEmail.Text = "Email Inválido";
+                campos = false;
+            }
+        }
+        if (string.IsNullOrEmpty(txtAssunto.Text))
+        {
+            panelAssunto.Visible = true;
+            erroAssunto.Text = "Obrigatório preenchimento do Assunto.";
+            campos = false;
+        }
+        else if (txtAssunto.Text.Length <= 7)
+        {
+            panelAssunto.Visible = true;
+            erroAssunto.Text = "Campo Assunto deve conter mais letras.";
+            campos = false;
+        }
+        else
+        {
+            panelAssunto.Visible = false;
+        }
+
+        if (string.IsNullOrEmpty(txtMensagem.Text))
+        {
+            panelMensagem.Visible = true;
+            erroMensagem.Text = "Obrigatório preenchimento da Mensagem.";
+            campos = false;
+        }
+        else if (txtMensagem.Text.Length <= 12)
+        {
+            panelMensagem.Visible = true;
+            erroAssunto.Text = "Campo Mensagem deve conter mais letras.";
+            campos = false;
+        }
+        else
+        {
+            panelMensagem.Visible = false;
+        }
+
+
+        if (campos)
+        {
+            EnviarEmail();
+        }
+        else
+        {
+            panelMsgEnviada.Visible = false;
+        }
+
+    }
+
+
+    private void EnviarEmail()
+    {
+        //Define os dados do e-mail
+        string nomeRemetente = "Portfolio Rafael -" + txtNome.Text;
+        string emailRemetente = "rafael.vidal@startagency.com.br";
+        string senha = "start2014";
+
+        //Host da porta SMTP
+        string SMTP = "smtp.startagency.com.br";
+
+        string emailDestinatario = "rafaelvieira12@hotmail.com";
+        string emailComCopia = "rafael.vidal@startagency.com.br";
+        //string emailComCopiaOculta  = "email@comcopiaoculta.com.br";
+
+        //string assuntoMensagem      = "Teste de envio de emails usando System.Net.Mail em C#";
+        //string conteudoMensagem     = "Teste de envio de emails usando System.Net.Mail em C#";
+
+        string CorpoEmail = ""
+                                + "<br>Data   : " + DateTime.UtcNow.ToString()
+                                + "<br>Assunto: " + txtAssunto.Text
+                                + "<br>Nome   : " + this.txtNome.Text.ToString()
+                                + "<br>Sobre Nome : " + this.txtSobreNome.Text.ToString()
+                                + "<br>E-Mail : " + this.txtEmail.Text.ToString()
+                                + "<br>Observ : " + this.txtMensagem.Text.ToString();
+
+
+        //Cria objeto com dados do e-mail.
+        MailMessage objEmail = new MailMessage();
+
+        //Define o Campo From e ReplyTo do e-mail.
+        objEmail.From = new System.Net.Mail.MailAddress(nomeRemetente + "<" + emailRemetente + ">");
+
+        //Define os destinatários do e-mail.
+        objEmail.To.Add(emailDestinatario);
+
+        //Enviar cópia para.
+        objEmail.CC.Add(emailComCopia);
+
+        //Enviar cópia oculta para.
+        //objEmail.Bcc.Add(emailComCopiaOculta);
+
+        //Define a prioridade do e-mail.
+        objEmail.Priority = System.Net.Mail.MailPriority.Normal;
+
+        //Define o formato do e-mail HTML (caso não queira HTML alocar valor false)
+        objEmail.IsBodyHtml = true;
+
+        //Define título do e-mail.
+        objEmail.Subject = txtAssunto.Text;
+
+        //Define o corpo do e-mail.
+        objEmail.Body = CorpoEmail;
+
+
+        //Para evitar problemas de caracteres "estranhos", configuramos o charset para "ISO-8859-1"
+        objEmail.SubjectEncoding = System.Text.Encoding.GetEncoding("ISO-8859-1");
+        objEmail.BodyEncoding = System.Text.Encoding.GetEncoding("ISO-8859-1");
+
+        System.Net.Mail.SmtpClient objSmtp = new System.Net.Mail.SmtpClient();
+
+        //Alocamos o endereço do host para enviar os e-mails  
+        objSmtp.Credentials = new System.Net.NetworkCredential(emailRemetente, senha);
+        //objSmtp.Host = SMTP;
+        objSmtp.Host = "localhost";
+        //objSmtp.Port = 587;
+        objSmtp.Port = 25;
+
+        //Caso utilize conta de email do exchange da locaweb deve habilitar o SSL
+        //objEmail.EnableSsl = true;
+
+        //Enviamos o e-mail através do método .send()
+        try
+        {
+            objSmtp.Send(objEmail);
+            //this.lbl_status.Text = "E-mail enviado com sucesso !";
+            txtNome.Text = "";
+            txtSobreNome.Text = "";
+            txtEmail.Text = "";
+            txtAssunto.Text = "";
+            txtMensagem.Text = "";
+            panelMsgEnviada.Visible = true;
+        }
+        catch (Exception ex)
+        {
+            this.lbl_status.Text = "Ocorreram problemas no envio do e-mail. Erro = " + ex.Message;
+        }
+        finally
+        {
+            //excluímos o objeto de e-mail da memória
+            objEmail.Dispose();
+            //anexo.Dispose();
+        }
+    }
+}
---
+++ js/scriptGeral.js
@@ -0,0 +1,174 @@
+﻿
+$(document).ready(function () {
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function (item) {
+                return item.el.attr('title') + '<small></small>';
+            }
+        }
+    });
+
+    /* Menu */
+    var inicio = 0;
+    $(".dropdown").click(function () {
+
+        if (inicio == 0) {
+            $(this).find("a > i").removeClass("fa-circle-o");
+            $(this).find("a > i").addClass("fa-dot-circle-o");
+            inicio = 1;
+        }
+        else {
+            $(this).find("a > i").removeClass("fa-dot-circle-o");
+            $(this).find("a > i").addClass("fa-circle-o");
+            inicio = 0;
+        }
+    });
+
+    /* Fechar menu */
+    $("#menu.navbar .nav > li").click(function () {
+        if (!$(this).attr("class")) {
+            $("#navbar").removeClass("in");
+        }
+    });
+    $(".dropdown-menu li a").click(function () {        
+        $("#navbar").removeClass("in");        
+    });
+    
+
+    /* Linha do tempo */
+    var alturaSecAcademico = $("#tela-academico > div").height();
+    var alturaSecprofissional = $("#tela-profissional > div").height();
+
+    $("#tela-academico .barra-centro > div").css("height", alturaSecAcademico)
+    $("#tela-profissional .barra-centro > div").css("height", alturaSecprofissional)
+
+    $(".bg-1").animate({ width: "10px" }, 1000, "linear", function () { });
+    $(".bg-2").animate({ width: "21px" }, 2000, "linear", function () { });
+    $(".bg-3").animate({ width: "27px" }, 3000, "linear", function () { });
+
+    setInterval(function () {
+        validaForm();
+    }, 500);
+
+
+
+    linkBox('PortfolioFront', 'portfolioFront');
+    linkBox('PortfolioDesign', 'portfolioDesign');
+    linkBox('Perfil', 'tela-perfil');
+    linkBox('Academico', 'tela-academico');
+    linkBox('Profissional', 'tela-profissional');
+    linkBox('FrontEnd', 'tela-front-end');
+    linkBox('Contato', 'tela-contato');
+    linkBox('Home', 'banner');
+
+
+    $(".linkPerfil").click(function () {
+        $("#imgPerfil").addClass("bounceInLeft");        
+    });
+});
+
+function resetAnimacao() {
+    $("#imgPerfil").removeClass("bounceInLeft");
+}
+
+function linkBox(nome, nome2) {
+    $(".link" + nome).click(function () {
+        $('html, body').animate({ scrollTop: $("#" + nome2).offset().top - 50 }, 2000);
+        resetAnimacao()
+    });
+}
+
+function modal(nomeModal) {
+    var largura = $(window).width();
+
+    if (largura < 765) {
+        $('html, body').animate({ scrollTop: $("#banner").offset().top }, 1000);
+    }
+   
+    $(".bg-modal").show();
+
+    for (var i = 0; i < $(".guarda-modal > div").length; i++) {
+        if ($(".guarda-modal > div").eq(i).attr("modal") == nomeModal) {
+            $(".guarda-modal > div").eq(i).toggle('slow');
+        }        
+    }
+}
+function fecharModal() {
+    $(".bg-modal").hide();
+    $(".guarda-modal > div").hide()
+}
+
+/* Valida E-mail*/
+function checkMail(mail) {
+    var er = new RegExp(/^[A-Za-z0-9_\-\.]+@[A-Za-z0-9_\-\.]{2,}\.[A-Za-z0-9]{2,}(\.[A-Za-z0-9])?/);
+    if (typeof (mail) == "string") {
+        if (er.test(mail)) { return true; }
+    } else if (typeof (mail) == "object") {
+        if (er.test(mail.value)) { return true; }
+    } else { return false; }
+}
+
+
+/* Valida E-mail*/
+
+function validaForm() {
+    var nome = $("input[name=txtNome]").val();
+    var sobreNome = $("input[name=txtSobreNome]").val();
+    var email = $("input[name=txtEmail]").val();
+    var assunto = $("input[name=txtAssunto]").val();
+    var mensagem = $("textarea[name=txtMensagem]").val();
+
+    if (nome.length > 5) {
+        $("#txtNome").addClass("corVerde");       
+    }
+    else {
+        $("#txtNome").removeClass("corVerde");
+    }
+    if (sobreNome.length > 5) {
+        $("#txtSobreNome").addClass("corVerde");
+    }
+    else {
+        $("#txtSobreNome").removeClass("corVerde");
+    }
+    if (checkMail(email)) {
+        $("#txtEmail").addClass("corVerde");        
+    }
+    else {
+        $("#txtEmail").removeClass("corVerde");        
+    }
+    if (assunto.length > 5) {
+        $("#txtAssunto").addClass("corVerde");        
+    }
+    else {
+        $("#txtAssunto").removeClass("corVerde");        
+    }
+    if (mensagem.length > 12) {
+        $("#txtMensagem").addClass("corVerde");
+    }
+    else {
+        $("#txtMensagem").removeClass("corVerde");
+    }
+    if (nome.length > 5 & checkMail(email) & assunto.length > 5 & mensagem.length > 12) {
+        $("#btEnviar").addClass("bg-verde");
+    }
+    else {
+        $("#btEnviar").removeClass("bg-verde");
+    }
+
+}
+
+/* Navegação */
+//
+
+
+    
